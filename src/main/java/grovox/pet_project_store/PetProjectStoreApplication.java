7b5fea485c6a7760f7f25f2ab1d50ac54e27f29f
Grovox <Grovox@bk.ru>
2024-05-28 14:44:32 +0300

    Create base spring project

---
+++ src/main/java/grovox/pet_project_store/PetProjectStoreApplication.java
@@ -0,0 +1,13 @@
+package grovox.pet_project_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProjectStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/grovox/pet_project_store/PetProjectStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package grovox.pet_project_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
